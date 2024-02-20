120cc979d8cda59f11fc0d9f96436e95fc9303b7
tahaamin2013 <tahaamindob2013@gmail.com>
2024-02-21 02:59:02 +0500

    sfdfsdfsdfsf

--- src/components/WhatWeDo.tsx
+++ src/components/WhatWeDo.tsx
@@ -17,42 +17,36 @@ const WhatWeDo: React.FC = () => {
     {
       image: "/23.png",
       title: "Step 1: Washing the Wheels",
-      time: 3000,
       description:
         "In the first step, attention is given to the wheels of the car. This involves a thorough cleaning process to remove dirt, brake dust, and other debris from the wheels, ensuring a fresh and polished look.",
     },
     {
       image: "/washing-the-weels.png",
       title: "Step 2: Darrin Carefully Prewashes the Lower Car",
-      time: 3000,
       description:
         "Darrin, presumably the car detailing professional, takes special care to prewash the lower parts of the car. This step is crucial for removing initial layers of grime and preparing the vehicle for a comprehensive cleaning.",
     },
     {
       image: "/full.jpg",
       title: "Step 3: Beginning of the Full Body Wash Even Before the Tunnel",
-      time: 3000,
       description:
         "The full-body wash commences, starting even before the car enters the cleaning tunnel. This stage involves a general surface cleaning to eliminate surface dirt and contaminants from the entire vehicle.",
     },
     {
       image: "/fsd.jpg",
       title: "Step 4: Full Body Hand Wash Before the Tunnel",
-      time: 3000,
       description:
         "Before entering the automated cleaning tunnel, a meticulous hand wash is performed on the entire body of the car. This step ensures a detailed cleaning, addressing specific areas that may require extra attention.",
     },
     {
       image: "/get.jpg",
       title: "Step 5: Getting Ready for the Pre-Soak Rinse",
-      time: 3000,
       description:
         "Preparations are made for the pre-soak rinse, a step that involves applying a specialized cleaning solution to loosen and break down stubborn dirt and grime before the main washing process.",
     },
     {
       image: "/rain.jpg",
       title: "Step 6: Working in the Rain! ",
-      time: 3000,
       description:
         "The car detailing process continues, even in adverse weather conditions such as rain. This may add an extra layer of challenge and precision to the detailing process, showcasing the dedication and expertise of the professional.",
     },
@@ -60,14 +54,12 @@ const WhatWeDo: React.FC = () => {
       image: "/rain.jpg",
       title:
         "Step 7: Inside the Tunnel with Another Hand Wash Using Lambswool Mittens",
-      time: 3000,
       description:
         "As the car enters the cleaning tunnel, another round of hand washing takes place. This time, lambswool mittens are utilized for a gentle yet effective cleaning, ensuring that every nook and cranny is thoroughly addressed.",
     },
     {
       image: "/rain.jpg",
       title: "Last Step: Teflon Wax Then Dry",
-      time: 3000,
       description:
         "In the final step, Teflon wax is applied to the car's surface. This not only enhances the vehicle's shine but also provides a protective layer against environmental elements. The car is then carefully dried, completing the detailing process and leaving it looking polished and well-maintained.",
     },
@@ -106,9 +98,7 @@ const WhatWeDo: React.FC = () => {
                   <div className="mt-4 flex flex-col flex-wrap ml-6">
                     <h1 className="text-left text-white">
                       <h1 className="font-bold text-2xl">{item.title}</h1>
-                      <p className="flex gap-2 font-bold text-sm mt-1">
-                        <Clock1 stroke="white" size={20} /> {item.time} min
-                      </p>
+                    
                       <p className="text-sm mt-3 mb-3">{item.description}</p>
                     </h1>
                   </div>
