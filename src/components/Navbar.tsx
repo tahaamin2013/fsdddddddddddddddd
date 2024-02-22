b19960d4c68569f30f8eb5a89b4dcdc15b6805ce
aimahusnain <aimahusnain@gmail.com>
2024-02-22 22:59:35 +0500

    sdfsdf

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,8 +40,10 @@ const Navbar = () => {
   return (
     // ${navbarColor}
 
-    <div className="bg-blue text-white mt-6 transition-all z-50 duration-200 shadow-xl px-5 lg:px-20 py-2 items-center justify-between fixed w-full"
-      style={{ backgroundImage: "url(/bubbles.png)" }}>
+    <div
+      className="bg-blue text-white mt-6 transition-all z-50 duration-200 shadow-xl px-5 lg:px-20 py-2 items-center justify-between fixed w-full"
+      style={{ backgroundImage: "url(/bubbles.png)" }}
+    >
       <div>
         <div className={`flex justify-between items-center`}>
           <Link href="/" className="outline-none absolute">
@@ -87,7 +89,7 @@ const Navbar = () => {
                     </SheetClose>
                   </div>
 
-                <SheetContent style={{ backgroundImage: "url(/bubbles.png)" }}>
+                  {/* <SheetContent style={{ backgroundImage: "url(/bubbles.png)" }}>
                   <SheetHeader>
                     <SheetDescription>
                       <div className="text-black font-bold flex flex-col text-left items-start gap-3 text-lg">
@@ -98,7 +100,7 @@ const Navbar = () => {
                         <Link href="/contact-us">Contact us</Link>
                       </div>
                     </SheetDescription>
-                  </SheetHeader>
+                  </SheetHeader> */}
                 </SheetContent>
               </Sheet>
             </div>
