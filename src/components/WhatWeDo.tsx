de035d3be2c511612505b1c9a05dfd5ac8edf641
aimahusnain <aimahusnain@gmail.com>
2024-02-25 10:27:27 +0500

    change the images urls

--- src/components/WhatWeDo.tsx
+++ src/components/WhatWeDo.tsx
@@ -21,35 +21,35 @@ const WhatWeDo: React.FC = () => {
     },
     {
       image:
-        "https://raw.githubusercontent.com/aimahusnain/Washme-CarWash-Images/main/washing-the-weels.png",
+        "https://raw.githubusercontent.com/aimahusnain/Washme-CarWash-Images/main/washing-the-weels.webp",
       title: "Step 2: Darrin Carefully Prewashes the Lower Car",
       description:
         "Darrin, the car detailing professional, takes special care to prewash the lower parts of the car. This step is crucial for removing initial layers of grime and preparing the vehicle for a comprehensive cleaning.",
     },
     {
       image:
-        "https://github.com/aimahusnain/Washme-CarWash-Images/blob/main/full.png?raw=true",
+        "https://raw.githubusercontent.com/aimahusnain/Washme-CarWash-Images/main/full%20(1).webp",
       title: "Step 3: Beginning of the Full Body Wash Even Before the Tunnel",
       description:
         "The full-body wash commences, starting even before the car enters the cleaning tunnel. This stage involves a general surface cleaning to eliminate surface dirt and contaminants from the entire vehicle.",
     },
     {
       image:
-        "https://github.com/aimahusnain/Washme-CarWash-Images/blob/main/fsd.png?raw=true",
+        "https://raw.githubusercontent.com/aimahusnain/Washme-CarWash-Images/main/fsd%20(2).webp",
       title: "Step 4: Full Body Hand Wash Before the Tunnel",
       description:
         "Before entering the automated cleaning tunnel, a meticulous hand wash is performed on the entire body of the car. This step ensures a detailed cleaning, addressing specific areas that may require extra attention.",
     },
     {
       image:
-        "https://github.com/aimahusnain/Washme-CarWash-Images/blob/main/get.png?raw=true",
+        "https://raw.githubusercontent.com/aimahusnain/Washme-CarWash-Images/main/get%20(1)%20(1).webp",
       title: "Step 5: Getting Ready for the Pre-Soak Rinse",
       description:
         "Preparations are made for the pre-soak rinse, a step that involves applying a specialized cleaning solution to loosen and break down stubborn dirt and grime before the main washing process.",
     },
     {
       image:
-        "https://github.com/aimahusnain/Washme-CarWash-Images/blob/main/in.png?raw=true",
+        "https://raw.githubusercontent.com/aimahusnain/Washme-CarWash-Images/main/in%20(1).webp",
       title:
         "Step 6: Inside the Tunnel with Another Hand Wash Using Lambswool Mittens",
       description:
@@ -57,7 +57,7 @@ const WhatWeDo: React.FC = () => {
     },
     {
       image:
-        "https://github.com/aimahusnain/Washme-CarWash-Images/blob/main/li.png?raw=true",
+        "https://raw.githubusercontent.com/aimahusnain/Washme-CarWash-Images/main/li.webp",
       title: "Last Step: Teflon Wax Then Dry",
       description:
         "In the final step, Teflon wax is applied to the car's surface. This not only enhances the vehicle's shine but also provides a protective layer against environmental elements. The car is then carefully dried, completing the detailing process and leaving it looking polished and well-maintained.",
